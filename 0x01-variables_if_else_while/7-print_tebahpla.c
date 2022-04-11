e160d33b38eb0b6055bed791dddd9683de357ecf
Yusuf Adewunmi <adewunmiyusuf14@gmail.com>
2022-04-11 12:38:15 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print alphabets in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'z';
+
+	while (a <= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
