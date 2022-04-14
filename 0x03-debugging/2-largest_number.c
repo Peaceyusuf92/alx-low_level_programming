9074f5263a9a5d81a93cad4690a0ee78af3e4c1e
Yusuf Adewunmi <adewunmiyusuf14@gmail.com>
2022-04-14 06:21:05 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,21 +11,24 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a < b && a > c)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (b > c && b > a)
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
-	else if (c > b)
+/*	else if (c > b)*/
+	else
 	{
 		largest = c;
 	}
-	else
-	{
-		largest = b;
-	}
+	/**
+	 * else
+	*{
+	*	largest = b;
+	*}
+	*/
 	return (largest);
 }
