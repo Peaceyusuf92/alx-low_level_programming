e4b6e5f8d0c739acf2e53f2f1e5431369e9d43a9
Yusuf Adewunmi <adewunmiyusuf14@gmail.com>
2022-04-21 21:50:11 -0700

    100 atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
+ */
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, x = 0, y = 1, z = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+		if (*(s + count) == '-')
+			y *= -1;
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				z *= 10;
+			size++;
+		}
+		count++;
+	}
+	for (i = count - size; i < count; i++)
+	{
+		x = x + ((*(s + i) - 48) * z);
+		z /= 10;
+	}
+	return (x * y);
+}
