eecb1369127d09ed3d1bb920f52b9f9ce2edf500
Yusuf Adewunmi <adewunmiyusuf14@gmail.com>
2022-04-23 22:02:41 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strcat - function commute strings
+ * @dest: parameter pointers to char
+ * @src: parameter pointer to char
+ * Return: return value to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
