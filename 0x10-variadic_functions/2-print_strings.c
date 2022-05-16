2518a2ec650c8c85dc90ebc2ed62f1d29088f449
Yusuf Adewunmi <adewunmiyusuf14@gmail.com>
2022-05-16 09:38:59 -0700

    print string followed by new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <strarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - print string variables
+ * @separator: char
+ * @n: unsigned int
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ax;
+	unsigned int a;
+	char *string;
+
+	va_start(ax, n);
+	for (a = 0; a < n, a++)
+	{
+		string = va_arg(ax, char *);
+		if (string)
+			printf("%s", string);
+		else
+			printf("(nil)");
+		if (a < n - 1 && separator)
+			printf("%s", separator);
+	}
+	va_end(ax);
+	printf("\n");
+}
