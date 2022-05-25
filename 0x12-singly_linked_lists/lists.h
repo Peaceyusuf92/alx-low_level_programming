eb65723014c61d8df7560bf12582d07aa4baafab
Yusuf Adewunmi <adewunmiyusuf14@gmail.com>
2022-05-25 14:09:58 -0700

    function for node in a linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,13 +6,15 @@
 #include <string.h>
 
 /**
- * struct list_s - single linked list
- * @str: string for malloc
- * @len: string lenght
- * @next: next node
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Description: singly linked list structure
+ * Description: singly linked list node structure
+ * for Holberton project
  */
+
 typedef struct list_s
 {
 	char *str;
