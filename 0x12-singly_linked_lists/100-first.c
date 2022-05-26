43186972020ccabe7eedcf5b6eaefd82f23781be
Yusuf Adewunmi <adewunmiyusuf14@gmail.com>
2022-05-25 17:23:29 -0700

    function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * ymain - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) ymain()
+{
+printf("You're beat! and yet, you must allow");
+printf(",\nI bore my house upon my back!\n");
+}
