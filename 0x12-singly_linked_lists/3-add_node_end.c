c6d7846685d143c98b990fc1ba097a390ee96329
Yusuf Adewunmi <adewunmiyusuf14@gmail.com>
2022-05-25 17:16:17 -0700

    function to add node to the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -32,9 +32,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	else
 	{
-	while (temp->next != NULL)
-		temp = temp->next;
+		while (temp->next != NULL)
+			temp = temp->next;
 		temp->next = new;
 	}
+
 	return (*head);
 }
