2ba0ede3dcd5170324a118a0db529cbf1153bfb1
Yusuf Adewunmi <adewunmiyusuf14@gmail.com>
2022-05-31 05:38:04 -0700

    binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int x;
-	int len, base_two;
+	int len, b_two;
 
 	if (!b)
 		return (0);
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (len--, b_two = 1; len >= 0; len--, b_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
@@ -28,7 +28,7 @@ unsigned int binary_to_uint(const char *b)
 
 		if (b[len] & 1)
 		{
-			ui += base_two;
+			x += b_two;
 		}
 	}
 
